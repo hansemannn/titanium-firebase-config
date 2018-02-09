71b913d62fd3b14414760a356f4fb604fe82e303
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-02-09 17:10:49 +0100

    Update app.js

--- example/app.js
+++ example/app.js
@@ -1,7 +1,1 @@
-// Require the Firebase CloudMessaging module
-var FirebaseCloudMessaging = require('firebase.cloudmessaging');
-
-// Configure FirebaseCloudMessaging
-Ti.API.info('FCM-Token :' + FirebaseCloudMessaging.fcmToken);
-
-// TODO: Write more examples based from the Readme.
+// TBD
